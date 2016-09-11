f54626391c14bdc921aff6cd981da3c370c5e44a
lilingyong <18922292506@163.com>
2016-09-11 12:19:23 +0800

    2:complete the slidemenu

--- app/src/main/java/com/liamhacker/com/material_design/slidemenu/SlideMenu.java
+++ app/src/main/java/com/liamhacker/com/material_design/slidemenu/SlideMenu.java
@@ -1,7 +1,10 @@
 package com.liamhacker.com.material_design.slidemenu;
 
+import android.app.Activity;
+
 /**
  * Created by lilingyong on 16/9/9.
  */
-public class SlideMenu {
+public class SlideMenu extends Activity{
+
 }
